4283cbbc6d851dfd74bcd63319bf907282e137b6
thiagobrsouza <tbs@outlook.com>
2021-04-29 11:47:24 -0300

    Endpoint/clientes/{id} disponivel

---
+++ src/main/java/com/cursomc/resources/ClienteResource.java
@@ -0,0 +1,27 @@
+package com.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursomc.domain.Cliente;
+import com.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Long id) {
+		
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
